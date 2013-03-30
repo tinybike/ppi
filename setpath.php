a0ce90792e734fc50ae42facd662ee8c69ee8e32
Jack Peterson <jack@tinybike.net>
2013-03-30 12:00:02 -0400

    hc datasets

--- setpath.php
+++ setpath.php
@@ -1,4 +1,5 @@
 <?php
 $server_root_1 = '/var/www/htdocs/ppi';
 $server_root_2 = '/home1/thefuns1/public_html/scrubbins';
-set_include_path(get_include_path().PATH_SEPARATOR.$server_root_1.PATH_SEPARATOR.$server_root_2);
+$server_root_3 = 'C:\\UniServer\\www\\ppi';
+set_include_path(get_include_path().PATH_SEPARATOR.$server_root_1.PATH_SEPARATOR.$server_root_2.PATH_SEPARATOR.$server_root_3);
