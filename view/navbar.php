70f3301f8cf74098e471653b259f32cac63bda0d
Jack Peterson <jack@tinybike.net>
2013-04-01 16:18:33 -0400

    title and intro lightbox

--- view/navbar.php
+++ view/navbar.php
@@ -5,62 +5,55 @@ function create_navbar() {
 		<div id="nav">
 			<nav>
 			<div class="tabs_table">
-				<table>
-				<tr>
-					<td>
-					<a id="try-1" class="try" href="#" onclick="return false;">about</a>								
-					</td>
-					<div id="how">
-					<td>
-					<a id="how-trigger" href="#" onclick="return false;">reference</a>
-					<div id="how-content">
-						<small>
-						J. Peterson, S. Presse, K. Peterson, and K. Dill. <a href="http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0039052">Simulated evolution of protein-protein interaction networks with realistic topology</a>. <i>PLoS ONE</i> 7(6): e39052, 2012.
-						</small>
-					</div>
-					</td>
-					<td>
-					<a href="https://github.com/tensorjack/ppi">source</a>
-					</td>
-					<td>
-					<a href="https://github.com/tensorjack/DUNE">simulation</a>
-					</td>
-					<td>
-					<a href="#" onclick="$(\'#intro\').lightbox_me({centered: true});/*$(\'#intro\').toggle(); $(\'#summary\').toggle();*/ return false;">help</a>
-					</td>
-					</div>
-				</tr>
-				</table>
+				<div id="title_label">
+					<table>
+						<tr>
+						<th><img src="images/interactome.png" alt="interactome" /></th>
+						</tr>
+					</table>
+				</div>
 			</div>
 			</nav>
 		</div>
+		<!---<nav>
 		<div id="title_label">
-		<img src="images/interactome.png" alt="interactome">
+			<table>
+				<tr><td>
+				<img src="images/interactome.png" alt="interactome" />
+				</td></tr>
+			</table>
 		</div>
+		</nav>--->
 		<nav>
 			<div class="tabs_table">
 				<div class="tabs">
-				<table>
-					<!---<div id="login">
+					<table>
 						<tr>
+						<div id="how">
 						<td>
-							<a id="login-trigger" href="#">search</a>
-							<div id="login-content">
-								<form action="index.php" method="post" class="form">
-									<fieldset id="inputs">
-										<input id="username" type="username" name="login_username" placeholder="Username" required />
-										<input id="password" type="password" name="login_password" placeholder="Password" required />
-									</fieldset>
-									<fieldset id="actions">
-										<input class="button" type="submit" id="submit" value="Search" />
-									</fieldset>
-								</form>
-							</div>
-						</td>--->	
+						<a href="#" onclick="$(\'#intro\').lightbox_me({centered: true});/*$(\'#intro\').toggle(); $(\'#summary\').toggle();*/ return false;">help</a>
+						</td>						
+						<!---<td>
+						<a id="how-trigger" href="#" onclick="return false;">reference</a>
+						<div id="how-content">
+							<small>
+							J. Peterson, S. Presse, K. Peterson, and K. Dill. <a href="http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0039052">Simulated evolution of protein-protein interaction networks with realistic topology</a>. <i>PLoS ONE</i> 7(6): e39052, 2012.
+							</small>
+						</div>
+						</td>--->
+						<td>
+						<a href="https://github.com/tensorjack/ppi">source</a>
+						</td>
+						<td>
+						<a href="https://github.com/tensorjack/DUNE">simulation</a>
+						</td>
 						<td><a href="http://dillgroup.org">dillgroup</a></td>
+						<td>
+						<a id="try-1" class="try" href="#" onclick="return false;">about</a>								
+						</td>
+						</div>
 						</tr>
-					</div>
-				</table>
+					</table>
 				</div>
 			</div>
 		</nav>
